18a77a20bc5f86e0226f55447373258f4d712bd0
Muhammadumar <khilolamoon@gmail.com>
2025-05-17 22:02:40 +0500

    attempt 2 to fix the bug

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -10,7 +10,9 @@ export default function OrdersPage() {
     <div className="flex flex-col justify-between min-h-screen p-4 bg-white border border-gray-200 rounded-xl">
       <div className="space-y-4">
         <p className="text-lg">List of orders</p>
-        <Tabs />
+        <Suspense fallback={<div>Loading filters...</div>}>
+          <Tabs />
+        </Suspense>
         <div className="flex items-center justify-between bg-gray-100 p-2 rounded-xl">
           <SearchBar />
           <Suspense fallback={<div>Loading filters...</div>}>
